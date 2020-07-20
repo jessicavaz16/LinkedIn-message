c57c74d5a4b8efa7506cc803f913899e21b27092
Jessica Vaz <56466050+jessicavaz16@users.noreply.github.com>
2020-07-21 00:46:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+
+var filbar = document.getElementById("filter");
+var filcon = document.getElementById("filter-icon");
+
+filcon.addEventListener("click",function(){
+    filbar.classList.toggle("hide");
+});
+
